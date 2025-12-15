90497c1778a5a500ac45c06731c6610e270a213f
Satellite-system <adarshurmaliya@gmail.com>
2025-12-15 18:41:15 +0530

    Add tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-undef */
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+    ibm: ['"IBM Plex Sans"', "sans-serif"],
+  }
+    },
+  },
+  plugins: [],
+}
