84871a78813a77fea82df236db3d7c1d95ba379a
Satellite-system <adarshurmaliya@gmail.com>
2025-12-15 18:43:54 +0530

    Upate helperFunctions

--- src/utils/helperFunctions.js
+++ src/utils/helperFunctions.js
@@ -2,7 +2,7 @@ export function calculateGenderPercentage(data) {
   let totalMale = 0;
   let totalFemale = 0;
 
-  data.forEach(item => {
+  data.forEach((item) => {
     totalMale += Number(item.male || 0);
     totalFemale += Number(item.female || 0);
   });
@@ -19,5 +19,36 @@ export function calculateGenderPercentage(data) {
   return {
     malePercentage: Number(((totalMale / total) * 100).toFixed(2)),
     femalePercentage: Number(((totalFemale / total) * 100).toFixed(2)),
+  };
+}
+
+export function getUtcDayRange(dateInput) {
+  const date = new Date(dateInput);
+
+  // Start of day (UTC)
+  const startUtc = Date.UTC(
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate(),
+    0,
+    0,
+    0,
+    0
+  );
+
+  // End of day (UTC)
+  const endUtc = Date.UTC(
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate(),
+    23,
+    59,
+    59,
+    999
+  );
+
+  return {
+    fromUtc: startUtc,
+    toUtc: endUtc,
   };
 }
