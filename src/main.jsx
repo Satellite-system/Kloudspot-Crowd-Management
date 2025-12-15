3b1d9f4cf8bdc84f6d4048e72eb43a9fea500abb
Satellite-system <adarshurmaliya@gmail.com>
2025-12-15 07:16:53 +0530

    Update main -> Added Context Providers

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthContext.jsx";
+import { DataCacheProvider } from "./context/DataCacheContext.jsx";
+import { GlobalProvider } from "./context/GlobalContext.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <DataCacheProvider>
+      <GlobalProvider>
+        <App />
+      </GlobalProvider>
+    </DataCacheProvider>
+  </AuthProvider>
+);
